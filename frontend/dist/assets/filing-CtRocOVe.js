5aff662916eaa3114b669f3644a5a20a27178f2b
mingchen <1906837163@qq.com>
2025-11-26 18:21:37 +0800

    v1.4.1版本更新

--- frontend/dist/assets/filing-CtRocOVe.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as s,c,a as n}from"./index-B_1xGcKF.js";const t={},a={class:"filing"};function r(_,e){return s(),c("div",a,e[0]||(e[0]=[n("p",null,"© 2025 DocTranslator 1.4.0 保留所有权利",-1)]))}const l=o(t,[["render",r],["__scopeId","data-v-c43c2b67"]]);export{l as F};
